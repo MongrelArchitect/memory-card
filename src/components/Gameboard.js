e0e82fbbf7dfedb42ff3b697a8079da96598b431
Sean Eric Thomas <set@seanericthomas.com>
2023-03-16 21:42:58 -0700

    Randomize gem ordering on click

--- src/components/Gameboard.js
+++ src/components/Gameboard.js
@@ -28,19 +28,35 @@ export default function Gameboard({ gems, setGems }) {
     return randomized;
   };
 
-  useEffect(() => {
-    // Randomize the gems on mount
+  const shuffleGems = () => {
     const gemsCopy = getGemsCopy(gems);
     const randomGems = randomizeArray(gemsCopy);
     setGems(randomGems);
+  };
+
+  const clickGem = () => {
+    shuffleGems();
+  };
+
+  useEffect(() => {
+    // Randomize the gems on mount
+    shuffleGems();
   }, []);
 
   return (
     <div>
       <h2>Gameboard</h2>
       {gems.map((gem) => (
-        <div className="gem" key={gems.indexOf(gem)}>
-          <img src={gem.image} alt={gem.name} />
+        <div
+          className="gem"
+          data-key={gems.indexOf(gem)}
+          key={gems.indexOf(gem)}
+          onClick={clickGem}
+          onKeyDown={clickGem}
+          role="button"
+          tabIndex={0}
+        >
+          <img src={gem.image} alt={`A cut and polished ${gem.name}`} />
           <span>{gem.name}</span>
         </div>
       ))}
