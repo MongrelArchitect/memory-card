fcad8aba06d54f0b2bb57fd1db4dcad41ad04099
Sean Eric Thomas <set@seanericthomas.com>
2023-03-16 17:48:21 -0700

    Display gems on gameboard

--- src/App.js
+++ src/App.js
@@ -1,17 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './components/Header';
 import Scoreboard from './components/Scoreboard';
 import Gameboard from './components/Gameboard';
 import Footer from './components/Footer';
+
+import placeholder from './images/placeholder.jpg';
+
 import './styles/reset.css';
 import './styles/style.css';
 
 export default function App() {
+  const [gems, setGems] = useState(
+    [
+      {
+        name: 'amethyst',
+        image: placeholder,
+        clicked: false,
+      },
+      {
+        name: 'diamond',
+        image: placeholder,
+        clicked: false,
+      },
+      {
+        name: 'citrene',
+        image: placeholder,
+        clicked: false,
+      },
+      {
+        name: 'emerald',
+        image: placeholder,
+        clicked: false,
+      },
+      {
+        name: 'sapphire',
+        image: placeholder,
+        clicked: false,
+      },
+      {
+        name: 'ruby',
+        image: placeholder,
+        clicked: false,
+      },
+    ],
+  );
+
   return (
     <div>
       <Header />
       <Scoreboard />
-      <Gameboard />
+      <Gameboard gems={gems} setGems={setGems} />
       <Footer />
     </div>
   );
--- src/components/Gameboard.js
+++ src/components/Gameboard.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
-export default function Gameboard() {
+export default function Gameboard({ gems }) {
   return (
     <div>
       <h2>Gameboard</h2>
+      {gems.map((gem) => (
+        <div className="gem" key={gems.indexOf(gem)}>
+          <img src={gem.image} alt={gem.name} />
+          <span>{gem.name}</span>
+        </div>
+      ))}
     </div>
   );
 }
